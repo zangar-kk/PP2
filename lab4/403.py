5c64430174b71b970fd8f677343805ab81ca1c23
zangar-kk <zakotrever@gmail.com>
2026-02-23 14:38:28 +0500

    Rename function to 'cheker' and update calls

--- lab4/403.py
+++ lab4/403.py
@@ -1,8 +1,9 @@
-def divisible_by_3_and_4(n):
+def cheker(n):
     for i in range(0, n + 1):
         if i % 3 == 0 and i % 4 == 0:
             yield i
 
 n = int(input())
 
-print(*divisible_by_3_and_4(n))
+
+print(*cheker(n))
