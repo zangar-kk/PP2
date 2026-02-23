c8336dcca215171291c96ca4a2bb2302a25f4d7f
zangar-kk <zakotrever@gmail.com>
2026-02-23 14:38:57 +0500

    Rename function even_numbers to eve

--- lab4/402.py
+++ lab4/402.py
@@ -1,12 +1,13 @@
-def even_numbers(n):
+def eve(n):
     for i in range(0, n + 1, 2):
         yield i
 
 n = int(input())
 
 first = True
-for num in even_numbers(n):
+for num in eve(n):
     if not first:
         print(",", end = "")
     print(num, end="")
+
     first = False
